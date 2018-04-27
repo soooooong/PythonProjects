acdd0fd67cc33119065792d536eb83680a6f808f
soooooong <644406543@qq.com>
2018-04-27 17:12:29 +0800

    save bug

--- hello/socket_module/client_for_servdemo.py
+++ hello/socket_module/client_for_servdemo.py
@@ -1,13 +1,10 @@
 __author__ = 'song'
-import  socket
+import socket
 #配套的client
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 建立连接:
-s.connect(('127.0.0.1', 9999))
-# 接收欢迎消息:
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('127.0.0.1',9999))
 print(s.recv(1024).decode('utf-8'))
-for data in [b'Michael', b'Tracy', b'Sarah']:
-    # 发送数据:
+for data in [b's',b'z']:
     s.send(data)
     print(s.recv(1024).decode('utf-8'))
 s.send(b'exit')
