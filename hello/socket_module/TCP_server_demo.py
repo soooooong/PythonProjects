22e836c46cf4026af929e2c82db2d02219ac8e7b
soooooong <644406543@qq.com>
2018-04-27 21:21:12 +0800

    change TCP_server

--- hello/socket_module/TCP_server_demo.py
+++ hello/socket_module/TCP_server_demo.py
@@ -1,6 +1,6 @@
 __author__ = 'song'
 import  threading
-import  socket
+import  socket,time
 #一个Socket依赖4项：服务器地址、服务器端口、客户端地址、客户端端口来唯一确定一个Socket
 
 #服务器还需要同时响应多个客户端的请求，
@@ -18,14 +18,6 @@ s.bind(('127.0.0.1', 9999))
 s.listen(5)#传入的参数指定等待连接的最大数量
 print('waiting for connection ...')
 
-#务器程序通过一个永久循环来接受来自客户端的连接，accept()会等待并返回一个客户端的连接
-while True:
-    #接受一个新链接
-    sock,addr=s.accept()
-    #创建新线程处理TCP链接
-    t=threading.Thread(target=tcplink,args=(sock,addr))
-
-
 #连接建立后，服务器首先发一条欢迎消息，
 # 然后等待客户端数据，并加上Hello再发送给客户端。
 # 如果客户端发送了exit字符串，就直接关闭连接
@@ -34,11 +26,24 @@ def tcplink(sock,addr):
     sock.send(b'welcome')
     while True:
         data = sock.recv(1024)
-        time.sleep(1)
+#        time.sleep(1)
         if not data or data.decode('utf-8') == 'exit':
             break
-        sock.send(('hello , %s'%data.decode('utf-8')).encode('utf-8'))
-        sock.close()
-        print('connection from %s:%s cleased'% addr)
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('connection from  cleased')
+
+
+#务器程序通过一个永久循环来接受来自客户端的连接，accept()会等待并返回一个客户端的连接
+while True:
+    #接受一个新链接
+    sock,addr=s.accept()
+    print(sock,addr)
+    print('accepted...')
+    #创建新线程处理TCP链接
+    t=threading.Thread(target=tcplink,args=(sock,addr))
+    t.start()
+
+
 
 ###############################################################
