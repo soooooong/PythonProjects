8bceeb2747681f87c3acdef3297dcae8139526ef
soooooong <644406543@qq.com>
2018-04-27 16:47:30 +0800

    TCP_demo

--- hello/socket_module/TCP_client_demo.py
+++ hello/socket_module/TCP_client_demo.py
@@ -32,3 +32,4 @@ print(header.decode('utf-8'))
 # 把接收的数据写入文件:
 with open('sina.html', 'wb') as f:
     f.write(html)
+
---
+++ hello/socket_module/TCP_server_demo.py
@@ -0,0 +1,56 @@
+__author__ = 'song'
+import  threading
+import  socket
+#一个Socket依赖4项：服务器地址、服务器端口、客户端地址、客户端端口来唯一确定一个Socket
+
+#服务器还需要同时响应多个客户端的请求，
+#所以，每个连接都需要一个新的进程或者新的线程来处理，否则，服务器一次就只能服务一个客户端了
+
+
+#创建一个基于IPv4和TCP协议的Socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#监听端口
+#用0.0.0.0绑定到所有的网络地址，还可以用127.0.0.1绑定到本机地址
+#小于1024的端口号必须要有管理员权限才能绑定,大于的可以自己使用
+s.bind(('127.0.0.1', 9999))
+s.listen(5)#传入的参数指定等待连接的最大数量
+print('waiting for connection ...')
+
+#务器程序通过一个永久循环来接受来自客户端的连接，accept()会等待并返回一个客户端的连接
+while True:
+    #接受一个新链接
+    sock,addr=s.accept()
+    #创建新线程处理TCP链接
+    t=threading.Thread(target=tcplink,args=(sock,addr))
+
+
+#连接建立后，服务器首先发一条欢迎消息，
+# 然后等待客户端数据，并加上Hello再发送给客户端。
+# 如果客户端发送了exit字符串，就直接关闭连接
+def tcplink(sock,addr):
+    print('accept new connection from %s:%s' % addr)
+    sock.send(b'welcome')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('hello , %s'%data.decode('utf-8')).encode('utf-8'))
+        sock.close()
+        print('connection from %s:%s cleased'% addr)
+
+###############################################################
+#配套的client
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接:
+s.connect(('127.0.0.1', 9999))
+# 接收欢迎消息:
+print(s.recv(1024).decode('utf-8'))
+for data in [b'Michael', b'Tracy', b'Sarah']:
+    # 发送数据:
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
+s.close()
