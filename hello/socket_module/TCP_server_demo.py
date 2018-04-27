a1909ff73010e8949710883b158c9fc33468e776
soooooong <644406543@qq.com>
2018-04-27 17:05:54 +0800

    new client

--- hello/socket_module/TCP_server_demo.py
+++ hello/socket_module/TCP_server_demo.py
@@ -42,15 +42,3 @@ def tcplink(sock,addr):
         print('connection from %s:%s cleased'% addr)
 
 ###############################################################
-#配套的client
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 建立连接:
-s.connect(('127.0.0.1', 9999))
-# 接收欢迎消息:
-print(s.recv(1024).decode('utf-8'))
-for data in [b'Michael', b'Tracy', b'Sarah']:
-    # 发送数据:
-    s.send(data)
-    print(s.recv(1024).decode('utf-8'))
-s.send(b'exit')
-s.close()
