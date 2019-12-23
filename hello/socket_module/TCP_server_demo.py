7a3e711452df0091b095a5e25279c35bca720692
song <644406543@qq.com>
2019-12-23 15:00:20 +0800

    无class 无处理
    无class 无消息处理

--- hello/socket_module/TCP_server_demo.py
+++ hello/socket_module/TCP_server_demo.py
@@ -4,12 +4,16 @@ import  socket,time
 # TCP和UDP都是全双工,在发送信息的同时,可以接收信息
 #一个Socket依赖4项：服务器地址、服务器端口、客户端地址、客户端端口来唯一确定一个Socket
 
+#python中list变量作为全局变量时，在函数中可以直接修改。
+#而普通变量则需要先在函数中global声明，否则会报错。
+conn_pool = []
 
 #用于广播的
 #假如端口被socket使用过，并且利用socket.close()来关闭连接，但此时端口还没有释放，要经过一个TIME_WAIT的过程之后才能使用，这是TNN的相当烦银的，为了实现端口的马上复用，可以选择setsockopt（）函数来达到目的
 ## 在绑定前调用 setsockopt 让套接字允许地址重用
 sock_broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_broadcast.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+##需要添加动态获取广播地址
 addr_broadcast = ('192.168.1.255', 7777)
 
 # send(),recv()用于TCP，sendto()及recvfrom（）用于UDP
@@ -42,7 +46,7 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #小于1024的端口号必须要有管理员权限才能绑定,大于的可以自己使用
 #s.bind(('127.0.0.1', 9999))
 s.bind(('0.0.0.0', 9999))
-s.listen(5)#传入的参数指定等待连接的最大数量
+s.listen(10)#传入的参数指定等待连接的最大数量
 print('waiting for connection ...')
 #连接建立后，服务器首先发一条欢迎消息，
 # 然后等待客户端数据，并加上Hello再发送给客户端。
@@ -52,13 +56,21 @@ def tcplink(sock,addr):
     sock.send(b'welcome')
     while True:
         data = sock.recv(1024)
-        print('data:%s' % data)
-        time.sleep(1)
+        print('recive data:%s' % data)
+
+        #客户端请求断开
         if not data or data.decode('utf-8') == 'exit':
+            conn_pool.remove(sock)
+            sock.close()
+            print('close Connection from %s:%s closed.' % addr)
             break
-        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
-    sock.close()
-    print('close Connection from %s:%s closed.' % addr)
+        else:
+        #客户端发送过来的消息
+            sock.send(('server recive: %s!' % data.decode('utf-8')).encode('utf-8'))
+
+    
+
+
 
 
 #服务器还需要同时响应多个客户端的请求，
@@ -67,6 +79,7 @@ def tcplink(sock,addr):
 while True:
     #接受一个新链接
     sock,addr=s.accept()
+    conn_pool.append(sock)
     print(sock,addr)
     print('accepted...')
     #创建新线程处理TCP链接
@@ -76,3 +89,4 @@ while True:
 
 
 ###############################################################
+ 
